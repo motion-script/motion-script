60d0d6f3accfcd54fcd4aaa8a7018d8dfdc0edd4
Kevin Oliveira <134442359+koliveiradev@users.noreply.github.com>
2026-06-19 17:39:22 -0400

    feat: persist player view changes

--- packages/template/src/projects/shapes/scenes/index.ts
+++
@@ -1,8 +0,0 @@
-export { ShapeScene } from './shape-scene';
-export type { ShapeSceneSpec, PropAnim } from './shape-scene';
-
-export { RectScene } from './rect-scene';
-export { EllipseScene } from './ellipse-scene';
-export { PolygonScene } from './polygon-scene';
-export { PolygramScene } from './polygram-scene';
-export { PathScene } from './path-scene';
--- packages/template/src/projects/video/scenes/index.ts
+++
@@ -1,8 +0,0 @@
-export { VideoBasicScene } from "./basic";
-export { VideoFitScene } from "./fit-mode";
-export { VideoCropScene } from "./crop-mode";
-export { VideoFilteredScene } from "./filtered";
-export { VideoPosterizedScene } from "./posterized";
-export { VideoEchoedScene } from "./echoed";
-export { VideoBlendedScene } from "./blended";
-export { VideoNodeScene } from "./video-node";
